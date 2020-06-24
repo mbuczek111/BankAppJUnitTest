d61f25e242fa980dd59a93e8b87a14f3db074a22
mikolaj <mbuczek111@gmail.com>
2020-06-24 15:06:39 +0200

    added more tests

--- src/test/java/BankTest.java
+++ src/test/java/BankTest.java
@@ -2,6 +2,9 @@ import bank.AccountNotFoundException;
 import bank.Bank;
 import bank.BankNotFoundException;
 import bank.NationalBank;
+import bank.account.Account;
+import bank.account.CreditAccount;
+import bank.account.DepositAccount;
 import org.hamcrest.MatcherAssert;
 import org.junit.jupiter.api.Test;
 
@@ -26,6 +29,26 @@ public class BankTest {
         nb.registerBank(alior);
         AccountNotFoundException exception = assertThrows(AccountNotFoundException.class, () -> nb.getByName("Alior Bank").getByNumber(1).transferMoney("Alior Bank", 4, BigDecimal.valueOf(0)));
         assertThat(exception.getMessage()).contains("Account not found");
-        // some comment
+    }
+    @Test
+    public void topUp_withAmountOfMoney_adds10000toAccountsBalance() {
+        NationalBank nb = NationalBank.getInstance();
+        Bank alior = new Bank("Alior Bank");
+        nb.registerBank(alior);
+        Account aliorCredit = new CreditAccount(BigDecimal.valueOf(0.1), BigDecimal.valueOf(-1000));
+        alior.addAccount(aliorCredit);
+        nb.getByName("Alior Bank").getByNumber(1).topUp(BigDecimal.valueOf(10000));
+        assertThat(aliorCredit.getBalance().toString()).contains("10000");
+    }
+    @Test
+    public void addAccount_incrementsAccountNumberByOne()
+    {
+        NationalBank nb = NationalBank.getInstance();
+        Bank alior = new Bank("Alior Bank");
+        Account aliorDeposit = new DepositAccount(BigDecimal.valueOf(0.05));
+
+        alior.addAccount(aliorDeposit);
+
+        assertThat(aliorDeposit.getAccountNumber()).isEqualTo(2);
     }
 }
